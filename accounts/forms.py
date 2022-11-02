d59bcc74483d183327d6da4bbff48ea6f0f8438b
juradosalado <juansaladous@gmail.com>
2022-11-02 10:49:03 +0100

    feat(#017): Create first version of register view

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user= super(RegisterForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+        return user
--- innoweb/urls.py
+++ innoweb/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from innoweb.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('accounts/', include('accounts.urls')),
 ]
