eb5b7511464754d3a9ee37a808d703b743f545e9
Tomas Camero <tcamerob@gmail.com>
2022-11-13 14:22:49 +0100

    feat(#30): Extended 'participante' entity with 'user' entity.
    I have extended the participant class, using the AbstractUser class and additionally adding the 'id' and 'photo' attributes. Then in admin.y I have created a class so that in django administracion all the fields come out. Additionally I have modified the login template by extending base.html

--- participant/models.py
+++ participant/models.py
@@ -1,24 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Participant(models.Model):
-    name = models.CharField(max_length=50, help_text="Introducce tu nombre")
-    surname = models.CharField(max_length=50, help_text="Introduce tus apellidos")
-    email = models.EmailField(max_length=50, help_text="Introduce tu correo")
+class Participant(AbstractUser):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     photo = models.URLField(max_length=200, help_text="Introduce la url de tu foto", blank=True)
     
-    #Metadata
-    class Meta:
-        ordering = ['name']
-    
-    #Métodos
-    def get_name(self):
-        return self.name
-
-    def get_surname(self):
-        return self.surname
-
-    def get_email(self):
-        return self.email
     
     
