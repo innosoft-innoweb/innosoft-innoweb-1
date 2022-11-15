17bf76f7a323a741faa2ccba1e31c240fda432ee
IsmaelP19 <ismperort@alum.us.es>
2022-11-15 10:01:02 +0100

    fix(#012): Fix podium
    Now the podium shows the first three participants of each event, instead of showing static information.

--- event/views.py
+++ event/views.py
@@ -9,8 +9,17 @@ def eventView(response, id):
     e = Event.objects.get(id=id)
     
     scores = Score.objects.filter(event=e).order_by('-value')
+    first = scores[0].participant
+    first.surname = first.surname[0] + '.'
+    second = scores[1].participant
+    second.surname = second.surname[0] + '.'
+    third = scores[2].participant  
+    third.surname = third.surname[0] + '.' 
     return render(response, "event/event_view.html", {
         "e": e, 
-        "scores": scores
+        "scores": scores,
+        "first": first,
+        "second": second,
+        "third": third
         })
 
--- innoweb/views.py
+++ innoweb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from score.models import Score
 
 # view for testing components
 def index(request):
@@ -18,6 +19,22 @@ def home(request):
     sample_events = [
         event, event, event, event, event, event, event, event, event
     ]
+
+    scores = Score.objects.order_by('-value')
+    first = scores[0].participant
+    first.surname = first.surname[0] + '.'
+    second = scores[1].participant
+    second.surname = second.surname[0] + '.'
+    third = scores[2].participant
+    third.surname = third.surname[0] + '.'
+
+
+
     
-    return render(request, 'base_HOME.html', {'events': sample_events})
+    return render(request, 'base_HOME.html', 
+        {'events': sample_events,
+        'first': first,
+        'second': second,
+        'third': third
+        })
     
