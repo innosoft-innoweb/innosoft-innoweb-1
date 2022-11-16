bff2a8a9a51eb0fb2674bb88299f4cc357c657b6
juradosalado <juansaladous@gmail.com>
2022-11-16 19:42:34 +0100

    feat(#017): Finish register view and implement register functionality with updated participant model
    I also added a new button in login.html that redirect to register.html.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from participant.models import Participant
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
-        model = User
         model = Participant
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'photo']
     
