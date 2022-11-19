253528e62f7dadb890e4e27ce8145f9704099eff
juradosalado <juansaladous@gmail.com>
2022-11-19 20:15:19 +0100

    feat(#041): Make base_HOME.hmtl dynamic loading events from database

--- innoweb/views.py
+++ innoweb/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from event.models import Event
+
 # view for testing components
 def index(request):
     return render(request, 'index.html')
@@ -9,19 +11,21 @@ def pruebaTablaParticipante(request):
 
 def home(request):
 
-    event = {
-                "name": "Evento",
-                "description": "Esto es un evento",
-                "date": "2023-01-01",
-                "place": "Lugar 1",
-                "image": "https://picsum.photos/300/300/?random",
-                "state": "Active",
-            }
+    events = Event.objects.all()
 
-    sample_events = [
-        event, event, event, event, event, event, event, event, event
-    ]
+    # event = {
+    #             "name": "Evento",
+    #             "description": "Esto es un evento",
+    #             "date": "2023-01-01",
+    #             "place": "Lugar 1",
+    #             "image": "https://picsum.photos/300/300/?random",
+    #             "state": "Active",
+    #         }
+
+    # sample_events = [
+    #     event, event, event, event, event, event, event, event, event
+    # ]
     
-    return render(request, 'base_HOME.html', {'events': sample_events})
+    return render(request, 'base_HOME.html', {'events': events})
     
 
