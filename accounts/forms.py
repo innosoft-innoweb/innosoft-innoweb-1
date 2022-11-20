da3662174cd668d238cc6dbb36f106430693e4f0
juradosalado <juansaladous@gmail.com>
2022-11-20 12:40:09 +0100

    fix(#017): Make participant email unique. Make first name and last name required

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ValidationError
 
 from participant.models import Participant
 
@@ -11,12 +12,3 @@ class RegisterForm(UserCreationForm):
         model = Participant
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'photo']
     
-    # def save(self, commit=True):
-    #     user= super(RegisterForm, self).save(commit=False)
-    #     user.first_name = self.cleaned_data['first_name']
-    #     user.last_name = self.cleaned_data['last_name']
-    #     user.email = self.cleaned_data['email']
-
-    #     if commit:
-    #         user.save()
-    #     return user
