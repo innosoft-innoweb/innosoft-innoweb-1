ca398f18d41a3d91b51fa8843ad302ec4739560e
juradosalado <juansaladous@gmail.com>
2022-11-20 11:59:33 +0100

    chore(#041): Delete commented code and display only open events

--- innoweb/views.py
+++ innoweb/views.py
@@ -11,20 +11,8 @@ def pruebaTablaParticipante(request):
 
 def home(request):
 
-    events = Event.objects.all()
-
-    # event = {
-    #             "name": "Evento",
-    #             "description": "Esto es un evento",
-    #             "date": "2023-01-01",
-    #             "place": "Lugar 1",
-    #             "image": "https://picsum.photos/300/300/?random",
-    #             "state": "Active",
-    #         }
-
-    # sample_events = [
-    #     event, event, event, event, event, event, event, event, event
-    # ]
+    
+    events = Event.objects.filter(status='Abierto')
     
     return render(request, 'base_HOME.html', {'events': events})
     
