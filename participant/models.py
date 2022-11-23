bdd26f58e72e1948ec882ba35ca49ca1a73411a0
ezegonmac <ezegonmac01@gmail.com>
2022-11-23 19:30:04 +0100

    test(#049): Add unit tests for Score models

--- participant/models.py
+++ participant/models.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Participant(AbstractUser):
-    email = models.EmailField(unique=True)
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     first_name = models.CharField(("first name"), max_length=150)
     last_name = models.CharField(("last name"), max_length=150)
-    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    email = models.EmailField(unique=True)
     photo = models.URLField(max_length=200, help_text="Introduce la url de tu foto", blank=True)
-    
-    
     
--- score/models.py
+++ score/models.py
@@ -7,8 +7,7 @@ e = importlib.import_module("event.models")
 
 # Create your models here.
 class Score(models.Model):
-    
-
     participant = models.ForeignKey(p.Participant, on_delete=models.CASCADE)
     event = models.ForeignKey(e.Event, on_delete=models.CASCADE)
     value = models.PositiveIntegerField(default=0, null=False, blank=False)
+    
