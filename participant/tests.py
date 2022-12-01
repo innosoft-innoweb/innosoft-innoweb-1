61792f8f60fd5ad2f4b7c59debaf2d869bf13969
migromarj <miguelromeroarjona@gmail.com>
2022-12-01 20:59:37 +0100

    chore(#46): Delete unused imports
    Co-authored-by: Tomas Camero <tcamerob@gmail.com>
    Co-authored-by: ezegonmac <ezegonmac01@gmail.com>

--- participant/tests.py
+++ participant/tests.py
@@ -1,9 +1,6 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 
 class ProfileViewTest(StaticLiveServerTestCase):
     fixtures = ['fixtures/initial.json']
