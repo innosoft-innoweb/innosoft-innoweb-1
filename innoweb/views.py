e346deac2762a60d64a34f73eaab13d721a3d89f
ezegonmac <ezegonmac01@gmail.com>
2022-12-06 14:43:18 +0100

    feat(#074): Homogenize app styles
    Add past and current events sections in home view.
    Add past events sections in profile view.
    Homogenize tables styles, create new components for those.
    Homogenize events list styles.
    Homogenize titles.
    Make sure all views are responsive.

--- innoweb/views.py
+++ innoweb/views.py
@@ -15,7 +15,14 @@ def index(request):
     return render(request, 'index.html')
 
 def home(request):
+<<<<<<< Updated upstream
     events = Event.objects.filter(status='Abierto')
+=======
+
+    future_events = Event.objects.filter(status='Abierto')
+    current_events = Event.objects.filter(status='En proceso')
+    past_events = Event.objects.filter(status='Finalizado')
+>>>>>>> Stashed changes
 
     if request.GET.get('logout') == 'logout':
        logout(request)
@@ -56,10 +63,13 @@ def home(request):
             first.username = first.get_username()
 
     return render(request, 'base_HOME.html', 
-        {'events': events,
-        'first': first,
-        'second': second,
-        'third': third
+        {
+            'current_events': current_events,
+            'past_events': past_events,
+            'future_events': future_events,
+            'first': first,
+            'second': second,
+            'third': third
         })
 
 
