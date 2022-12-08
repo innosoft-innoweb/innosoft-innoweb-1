8ff78ae3590ec841247209034d063ac5221be2ec
migromarj <miguelromeroarjona@gmail.com>
2022-12-08 21:35:21 +0100

    fix(#75): Test changes to fix GitHub Actions bugs
    Tests run by GitHub Actions were not correctly accessing the value of the user password environment variable.
    A user has been added in the initial.json file for tests that require login.
    Remove an environment variable from the Github Actions file as it will no longer be used.

--- innoweb/tests.py
+++ innoweb/tests.py
@@ -5,9 +5,6 @@ from event.models import Event
 from score.models import Score
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from decouple import config
-
-USER_PASSWORD = config('USER_PASSWORD')
 
 class HomeViewTest(StaticLiveServerTestCase):
     fixtures = ['fixtures/initial.json']
@@ -131,8 +128,8 @@ class LoginViewTest(StaticLiveServerTestCase):
     
     def test_login_success(self):
         
-        username = "tomcambor"
-        password = USER_PASSWORD
+        username = "userTest"
+        password = "Testpassword17"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
         self.browser.get("http://localhost:" + PORT + "/login")
@@ -149,7 +146,7 @@ class LoginViewTest(StaticLiveServerTestCase):
         
     def test_login_username_fail(self):
         username = "incorrectusername"
-        password = USER_PASSWORD
+        password = "Testpassword17"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
         self.browser.get("http://localhost:" + PORT + "/login")
@@ -164,7 +161,7 @@ class LoginViewTest(StaticLiveServerTestCase):
         assert len(self.browser.find_elements("id","error-form")) == 1
         
     def test_login_password_fail(self):
-        username = "tomcambor"
+        username = "userTest"
         password = "incorrectpassword"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
@@ -196,8 +193,8 @@ class EventViewTest(StaticLiveServerTestCase):
         super(EventViewTest, cls).tearDownClass()
     
     def test_register_event(self):
-        username = "tomcambor"
-        password = USER_PASSWORD
+        username = "userTest"
+        password = "Testpassword17"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
         self.browser.get("http://localhost:" + PORT + "/login")
@@ -217,8 +214,8 @@ class EventViewTest(StaticLiveServerTestCase):
         assert self.browser.find_element(By.CLASS_NAME,"alert-success").is_enabled() == True
     
     def test_register_event_already_registered(self):
-        username = "tomcambor"
-        password = USER_PASSWORD
+        username = "userTest"
+        password = "Testpassword17"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
         self.browser.get("http://localhost:" + PORT + "/login")
@@ -266,8 +263,8 @@ class ProfileViewTest(StaticLiveServerTestCase):
         super(ProfileViewTest, cls).tearDownClass()
     
     def test_next_events_are_shown(self):
-        username = "tomcambor"
-        password = USER_PASSWORD
+        username = "userTest"
+        password = "Testpassword17"
         PORT = self.live_server_url.split(":")[2]
         self.browser.get(self.live_server_url)
         self.browser.get("http://localhost:" + PORT + "/login")
